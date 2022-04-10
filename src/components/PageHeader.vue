b13b6ef9487cacc82b8581390d7431137f2c020e
Wielton <grantwielgosz@gmail.com>
2022-04-09 19:56:13 -0600

    Initial commit with beginning structure

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Let's throw some elements onto the page</p>
+        <nav></nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PageHeader'
+        }
+</script>
+
+<style scoped>
+
+</style>
